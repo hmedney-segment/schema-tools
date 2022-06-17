8c357faf7ad9b20bc9416866d8a15a131106953c
Hunter Medney <hmedney@gmail.com>
2022-06-16 20:42:31 -0400

    render object and object array schemas

--- doc-site/pages/events/[slug].jsx
+++ doc-site/pages/events/[slug].jsx
@@ -1,5 +1,5 @@
 import {getEvents} from '../../lib/schema';
-import {Table, TableHead, TableHeader, TableBody, TableRow, TableCell} from '@carbon/react';
+import {Tag, Table, TableHead, TableHeader, TableBody, TableRow, TableCell} from '@carbon/react';
 
 export function getStaticPaths() {
   const events = getEvents();
@@ -17,27 +17,59 @@ export function getStaticProps({params}) {
   return {props: {event}};
 }
 
-function PropTable({title, props}) {
+function PropTable({title, props, showHeader = true, className, size = 'lg'}) {
+  function getSortKey(prop) {
+    return `${prop.required === true ? '0' : '1'}${prop.name}`;
+  }
+  const sortedProps = props.sort((p1, p2) => getSortKey(p1).localeCompare(getSortKey(p2)));
+
   return (
-    <div className="mt-5">
-      <h4>{title}</h4>
-      <Table className="w-auto">
-        <TableHead>
-          <TableRow>
-            <TableHeader>Name</TableHeader>
-            <TableHeader>Type</TableHeader>
-            <TableHeader>Description</TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.map((prop) => (
-            <TableRow key={props.name}>
-              <TableCell>
-                <strong>{prop.name}</strong>
-              </TableCell>
-              <TableCell>{prop.type}</TableCell>
-              <TableCell>{prop.description}</TableCell>
+    <div className={className}>
+      {title && <h4>{title}</h4>}
+      <Table className="w-auto" size={size}>
+        {showHeader && (
+          <TableHead>
+            <TableRow>
+              <TableHeader>Name</TableHeader>
+              <TableHeader>Type</TableHeader>
+              <TableHeader>Description</TableHeader>
+              <TableHeader></TableHeader>
             </TableRow>
+          </TableHead>
+        )}
+        <TableBody>
+          {sortedProps.map((prop) => (
+            <>
+              <TableRow key={prop.name}>
+                <TableCell>
+                  <strong>{prop.name}</strong>
+                </TableCell>
+                <TableCell>{prop.type}</TableCell>
+                <TableCell>{prop.description}</TableCell>
+                <TableCell> {prop.required === true && <Tag type="teal">Required</Tag>}</TableCell>
+              </TableRow>
+
+              {/* render object array schema */}
+              {prop.type === 'array' && prop.items?.properties && (
+                <TableRow className="mt-0">
+                  <TableCell colSpan="4" className="ps-4">
+                    <p>
+                      Elements of <code>{prop.name}</code> array:
+                    </p>
+                    <PropTable props={Object.values(prop.items.properties)} showHeader={false} size="sm" />
+                  </TableCell>
+                </TableRow>
+              )}
+
+              {/* render object schema */}
+              {prop.type === 'object' && prop.properties && (
+                <TableRow className="mt-0">
+                  <TableCell colSpan="4" className="ps-4">
+                    <PropTable props={Object.values(prop.properties)} showHeader={false} size="sm" />
+                  </TableCell>
+                </TableRow>
+              )}
+            </>
           ))}
         </TableBody>
       </Table>
@@ -50,8 +82,8 @@ export default function EventPage({event}) {
     <div>
       <h1>{event.title}</h1>
       <p>{event.description}</p>
-      <PropTable title="Event Properties" props={event.properties} />
-      {event.context && <PropTable title="Context Properties" props={event.context} />}
+      <PropTable title="Event Properties" props={event.properties} className="mt-5" />
+      {event.context && <PropTable title="Context Properties" props={event.context} className="mt-5" />}
     </div>
   );
 }
