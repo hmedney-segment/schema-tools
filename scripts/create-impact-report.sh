7c46c54de31eff82a7763cbababe96f3c61f7f6d
Hunter Medney <hmedney@gmail.com>
2022-06-18 20:07:48 -0400

    suppress init stdout

--- scripts/create-impact-report.sh
+++ scripts/create-impact-report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /usr/app/scripts/init.sh
+source /usr/app/scripts/init.sh > /dev/null
 
 # create markdown diff report
 node /usr/app/tools/commands/pr-checks/build-impact-report.js
